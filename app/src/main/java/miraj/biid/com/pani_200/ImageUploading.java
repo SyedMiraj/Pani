71a8dc4404b02f07e5073dc763bd92570644211a
smiraj <smiraj2507@gmail.com>
2018-01-17 12:32:51 +0600

    update the ImageUploading page with photo capturing and selecting image option added

--- app/src/main/java/miraj/biid/com/pani_200/ImageUploading.java
+++ app/src/main/java/miraj/biid/com/pani_200/ImageUploading.java
@@ -1,15 +1,24 @@
 package miraj.biid.com.pani_200;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+import com.kosalgeek.android.photoutil.CameraPhoto;
 import com.kosalgeek.android.photoutil.GalleryPhoto;
+import com.kosalgeek.android.photoutil.PhotoLoader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Shahriar Miraj on 15/11/2017.
@@ -19,8 +28,12 @@ public class ImageUploading extends AppCompatActivity implements View.OnClickLis
     public static TextView camera,addImg;
     Toolbar toolbar;
     GalleryPhoto galleryPhoto;
-    ArrayList<FieldImageModel> fieldImageModelList=new ArrayList<>();
-    private final int GALLERY_REQUEST=1200;
+    CameraPhoto cameraPhoto;
+    LinearLayout linearImage;
+    ImageView imageView;
+    private final int GALLERY_REQUEST = 1200;
+    private final int CAMERA_REQUEST = 1500;
+    List<String> imageList =  new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +48,11 @@ public class ImageUploading extends AppCompatActivity implements View.OnClickLis
     }
 
     private void init(){
-        camera= (TextView) findViewById(R.id.camera);
-        addImg= (TextView) findViewById(R.id.addimg);
-        galleryPhoto=new GalleryPhoto(getApplicationContext());
+        camera = (TextView) findViewById(R.id.camera);
+        addImg = (TextView) findViewById(R.id.addimg);
+        cameraPhoto = new CameraPhoto(getApplicationContext());
+        galleryPhoto = new GalleryPhoto(getApplicationContext());
+        linearImage = (LinearLayout) findViewById(R.id.linearImage);
     }
 
     @Override
@@ -53,33 +68,110 @@ public class ImageUploading extends AppCompatActivity implements View.OnClickLis
     }
 
     private void imageCapturing(){
-
+        try {
+            startActivityForResult(cameraPhoto.takePhotoIntent(),CAMERA_REQUEST);
+            cameraPhoto.addToGallery();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void addImageInList() {
-
         Intent intent=galleryPhoto.openGalleryIntent();
         startActivityForResult(intent,GALLERY_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode==0){
-            switch (resultCode){
-                case Activity.RESULT_OK:
-
-                    break;
-                case Activity.RESULT_CANCELED:
-                    Toast.makeText(getApplicationContext(),"Image captured failed",Toast.LENGTH_SHORT).show();
-                    break;
-                default:
-                    break;
+        if(resultCode == RESULT_OK){
+            if(requestCode == CAMERA_REQUEST){
+                String photoPath = cameraPhoto.getPhotoPath();
+                addImageToInterface(photoPath);
             }
-        }
-        if(requestCode==GALLERY_REQUEST && data.getData() != null){
-
+            if(requestCode == GALLERY_REQUEST && data.getData() != null){
+                Uri uri = data.getData();
+                galleryPhoto.setPhotoUri(uri);
+                String photoPath = galleryPhoto.getPath();
+                addImageToInterface(photoPath);
+            }
         }
     }
 
+    public void addImageToInterface(String imagePath){
+        if(imagePath != null){
+            imageList.add(imagePath);
+            try {
+                Bitmap bitmap = PhotoLoader.init().from(imagePath).requestSize(512, 512).getBitmap();
+                imageView = new ImageView(getApplicationContext());
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT);
+                imageView.setLayoutParams(layoutParams);
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setPadding(20, 0, 20, 10);
+                imageView.setAdjustViewBounds(true);
+                imageView.setImageBitmap(bitmap);
+                linearImage.addView(imageView);
+                imageView.setOnClickListener(this);
+                playTheAudio();
+            }catch(Exception e){
+
+            }
+        }
+    }
+
+    private void playTheAudio()
+    {
+        try {
+            if (imageList.size() == 1) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.one);
+                mPlayer.start();
+            } else if (imageList.size() == 2) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.two);
+                mPlayer.start();
+            } else if (imageList.size() == 3) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.three);
+                mPlayer.start();
+            } else if (imageList.size() == 4) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.four);
+                mPlayer.start();
+            } else if (imageList.size() == 5) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.five);
+                mPlayer.start();
+            } else if (imageList.size() == 6) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.six);
+                mPlayer.start();
+            } else if (imageList.size() == 7) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.seven);
+                mPlayer.start();
+            } else if (imageList.size() == 8) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.eight);
+                mPlayer.start();
+            } else if (imageList.size() == 9) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.nine);
+                mPlayer.start();
+            } else if (imageList.size() == 10) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.ten);
+                mPlayer.start();
+            } else if (imageList.size() == 11) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.eleven);
+                mPlayer.start();
+            } else if (imageList.size() == 12) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.twelve);
+                mPlayer.start();
+            } else if (imageList.size() == 13) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.thirtin);
+                mPlayer.start();
+            } else if (imageList.size() == 14) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.fortin);
+                mPlayer.start();
+            } else if (imageList.size() == 15) {
+                MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.fiftin);
+                mPlayer.start();
+            }
+        }catch (Exception e)
+        {
+            Log.i("GCA", "Exception: "+e);
+        }
+    }
 
 }
